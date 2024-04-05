254b800ea9a1f03aac06759880192c2f68e688fa
roshanb <dinendraroshan@gmail.com>
2024-04-05 22:14:03 +0530

    First commit

---
+++ arithmetic_operations.py
@@ -0,0 +1,15 @@
+def add(x,y):
+    return x+y
+
+def sub(x,y):
+    return x-y
+
+def multiply(x,y):
+    return x*y
+
+def divide(x,y):
+    if x != 0:
+        return x/y
+    
+    else:
+        return "Error division by zero"
---
+++ main.py
@@ -0,0 +1,7 @@
+import arithmetic_operations 
+
+# Test the arithmetic operations
+print("Addition:", arithmetic_operations.add(5, 3))
+print("Subtraction:", arithmetic_operations.sub(5, 3))
+print("Multiplication:", arithmetic_operations.multiply(5, 3))
+print("Division:", arithmetic_operations.divide(5, 3))
