87ce3e4a7c154b20dffc50051dcf7380b641d641
roshanb <dinendraroshan@gmail.com>
2024-04-05 22:25:07 +0530

    advance operations

---
+++ advance_operations.py
@@ -0,0 +1,8 @@
+import math
+
+def squre_root(x):
+    return math.sqrt(x)
+
+
+def power(x,y):
+    return math.pow(x,y)
--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import arithmetic_operations 
+import advance_operations
 
 # Test the arithmetic operations
+print("**********Test the arithmetic operations**********")
 print("Addition:", arithmetic_operations.add(5, 3))
 print("Subtraction:", arithmetic_operations.sub(5, 3))
 print("Multiplication:", arithmetic_operations.multiply(5, 3))
 print("Division:", arithmetic_operations.divide(5, 3))
+
+
+#Test the advance operations
+print("***********Test the advance operations*************")
+print("Squre_root:",advance_operations.squre_root(9))
+print("Squre_root:",advance_operations.power(2,9))
